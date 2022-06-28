369b0cf7c5f02106e5efc554b79ed7da3c165adc
P. Alexander Burnham <pburnham@uvm.edu>
2022-06-28 14:58:48 -0400

    v0.0.02
    fixed known issues with the cube smasher function. Passing functions now returns a cube object unless a parent cube is omited

--- spacetime/operations/cubeSmasher.py
+++ spacetime/operations/cubeSmasher.py
@@ -1,6 +1,7 @@
 import numpy as np
 import netCDF4 as nc
 from spacetime.objects.interumCube import interum_cube
+import xarray as xr
 
 def cube_smasher(function = None, eq = None, parentCube = None, **kwarg):
 
@@ -11,7 +12,6 @@ def cube_smasher(function = None, eq = None, parentCube = None, **kwarg):
         else:
             filestovar = True
 
-
     # loop through input dict to extract raster data for operations
     for key in kwarg:
         if "list" in str(type(kwarg[key])):
@@ -21,18 +21,32 @@ def cube_smasher(function = None, eq = None, parentCube = None, **kwarg):
         else:
             if "cube" in str(type(kwarg[key])):
                 kwarg[key] = kwarg[key].get_raster_data()
+
     # do operations as below
     if function == None:
-
         y = eval(eq, kwarg)
 
-    if eq == None:
 
+    if eq == None:
         y = function(**kwarg)
 
     if parentCube == None:
         out = y
+
     else:
+        time = parentCube.extract_time()
+        lon = parentCube.get_latitude()
+        lat = parentCube.get_longitude()
+        variables = parentCube.get_names()
+
+        c = np.where(parentCube.get_raster_data() == parentCube.get_nodata_value(), parentCube.get_nodata_value(), y) #, parentCube.get_raster_data(), y
+
+        y = xr.DataArray(data=c, dims=["variables" ,"lon", "lat", "time"], coords=dict(
+            variables = (["variables"], variables),
+            lon=(["lon"], lon),
+            lat=(["lat"], lat),
+            time=time))
+
         out = interum_cube(cube = parentCube, array = y, structure = filestovar)
 
     return out
