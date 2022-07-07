2b4645e9328ad9324d4096f85dc70c2129649be1
P. Alexander Burnham <pburnham@uvm.edu>
2022-07-07 12:15:50 -0400

    v0.0.03
    added ability to trim rasters by corners, shape files or union/intersection

--- spacetime/scale/rasterTrim.py
+++ spacetime/scale/rasterTrim.py
@@ -21,8 +21,7 @@ from spacetime.objects.fileObject import file_object
 # for the upper left and lower right corners and the GDAL geotransform output vector
 ######################################################################################################################
 
-def raster_trim(data=None):
-
+def raster_trim(data = None, method = "intersection", ul = None, lr = None, shapeFile = None):
 
     outList = [] # initialize a list
 
@@ -40,7 +39,7 @@ def raster_trim(data=None):
         # get meta data for each raster
         meta = gdal.Info(rastList[i])
 
-        # use regex to extract upper and lower left corners
+        # use regex to extract upper and lower right corners
         Uleft = re.search(r'Upper Left  \(([^)]+)', meta).group(1)
         Lright = re.search(r'Lower Right \(([^)]+)', meta).group(1)
 
@@ -58,23 +57,32 @@ def raster_trim(data=None):
 
     # find greatest common dimensions to crop rasters to:
     # highest x, lowest y (upper left corner): lowest x, highest y (lower right corner)
-    cornersCommon = [np.max(cornerArray[:,0]), np.min(cornerArray[:,1]), np.min(cornerArray[:,2]), np.max(cornerArray[:,3])]
+    if method == "intersection":
+        cornersCommon = [np.max(cornerArray[:,0]), np.min(cornerArray[:,1]), np.min(cornerArray[:,2]), np.max(cornerArray[:,3])]
 
-    #p1 = point upper left of bounding box
-    #p2 = point bottom right of bounding box
-#     p1 = (cornersCommon[0], cornersCommon[1])
-#     p2 = (cornersCommon[2], cornersCommon[3])
+    if method == "union":
+        cornersCommon = [np.min(cornerArray[:,0]), np.max(cornerArray[:,1]), np.max(cornerArray[:,2]), np.min(cornerArray[:,3])]
 
-    ulx=cornersCommon[0]
-    uly=cornersCommon[1]
-    lrx=cornersCommon[2]
-    lry=cornersCommon[3]
+    if method == "corners":
+        cornersCommon = ul + lr
 
     # loop to find common bounding box
     for i in range(len(rastList)):
 
         # trim the matrix
-        trimData = gdal.Translate('', rastList[i], projWin=[ulx, uly, lrx, lry], format='VRT')
+        if shapeFile != None and method == "shape":
+
+            trimData = gdal.Warp('', rastList[i], cutlineDSName = shapeFile, cropToCutline=True, format='VRT')
+
+        else:
+
+            # common corners
+            ulx=cornersCommon[0]
+            uly=cornersCommon[1]
+            lrx=cornersCommon[2]
+            lry=cornersCommon[3]
+
+            trimData = gdal.Translate('', rastList[i], projWin = [ulx, uly, lrx, lry], format='VRT')
 
         # append the data layers to the list
         outList.append(trimData)
