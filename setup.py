3810dfe55402ceb80068b887a20431b1c0636fd3
P. Alexander Burnham <pburnham@uvm.edu>
2023-04-20 09:38:28 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='spacetime',
     version='0.0.1',
-    packages=['spacetime_demo'],
+    packages=find_packages()
     url='biobarracuda.com',
     license='GNU GPLv3',
     author='P. A. Burnham et al.',
